653fb5f54436dd13fee6e7a8f5cace03d5ea94a4
will0225 <51754234+will0225@users.noreply.github.com>
2025-04-02 20:44:43 -0400

    update for server reconnection

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 
 require('dotenv').config();
 
+function connectToDatabase() {
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -14,4 +16,18 @@ connection.connect((err) => {
     console.log('Connected to MySQL database');
 });
 
+return connection;
+
+}
+
+let connection = connectToDatabase();
+
+// If connection is closed, reconnect
+connection.on('error', function(err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('Connection lost. Reconnecting...');
+      connection = connectToDatabase(); // Reconnect on lost connection
+    }
+  });
+  
 module.exports = connection;
